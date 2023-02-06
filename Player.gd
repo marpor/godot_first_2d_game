18395fa38d3212c80446df7d4562d3bb1d70d321
Mark Porsby <mark@porsby.dk>
2023-02-06 18:29:14 +0100

    Coding the player. Add Script

---
+++ Player.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
+
+func _ready():
+	screen_size = get_viewport_rect().size
