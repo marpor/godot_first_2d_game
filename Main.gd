2bfd3bac1967f26ee307a73e177c8826d0af5a2d
Mark Porsby <mark@porsby.dk>
2023-02-06 19:24:32 +0100

    Heads up display. Removing old creeps

--- Main.gd
+++ Main.gd
@@ -17,6 +17,7 @@ func new_game():
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 func _on_ScoreTimer_timeout():
 	score += 1
