8fb0eb92f58226f727293546b9f8ad1b698860aa
Mark Porsby <mark@porsby.dk>
2023-02-06 18:35:26 +0100

    Coding the player. Choosing animations

--- Player.gd
+++ Player.gd
@@ -27,3 +27,12 @@ func _process(delta):
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		# See the note below about boolean assignment.
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
+
