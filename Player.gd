460e767e5ed6e8a6a2416bbb27fde4a00a311fe7
Mark Porsby <mark@porsby.dk>
2023-02-06 18:36:12 +0100

    Coding the player. Preparing for collisions

--- Player.gd
+++ Player.gd
@@ -1,10 +1,12 @@
 extends Area2D
+signal hit
 
 export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
@@ -36,3 +38,13 @@ func _process(delta):
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
 
+func _on_Player_body_entered(body):
+	hide() # Player disappears after being hit.
+	emit_signal("hit")
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
